3473fdf971369bdbcc889c12df11db0807f5e792
Allison Ricker <alliricker@gmail.com>
2020-10-01 21:32:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supplies|
       if supplies.include?("BBQ")
-        return holiday.flatten.compact
+        return holiday
       end
     end
   end
